162fa2596c5b4c49bc7fe83fd62c72428fa46cb2
Thomas Mullaly <thomas.mullaly@gmail.com>
2015-10-14 22:53:28 -0500

    Added basic build system

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+    "use strict";
+    const srcDir = "src";
+    const appDir = `${srcDir}/app`;
+    const bowerComponentsDir = `${appDir}/bower_components`;
+    const buildDir = "build";
+    const distDir = `${buildDir}/dist`;
+    const distSrcDir = `${distDir}/src`;
+    const distAppDir = `${distSrcDir}/app`;
+    const distBowerComponents = `${distAppDir}/bower_components`;
+
+    let copyConfig = {
+        bower: {
+            files: [{
+                expand: true,
+                cwd: bowerComponentsDir,
+                src: ["**"],
+                dest: distBowerComponents
+            }]
+        },
+        electronPackage: {
+            files: [{
+                expand: true,
+                src: ["package.json"],
+                dest: `${distDir}`
+            }]
+        }
+    };
+
+    let babelConfig = {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: [{
+                expand: true,
+                cwd: srcDir,
+                src: ["**/*.js", "!app/bower_components/**/*.js"],
+                dest: distSrcDir,
+                ext: ".js"
+            }]
+        }
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        clean: [buildDir],
+        babel: babelConfig,
+        copy: copyConfig
+    });
+
+    grunt.loadNpmTasks("grunt-babel");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.registerTask("default", ["clean", "babel", "copy:bower", "copy:electronPackage"]);
+};
